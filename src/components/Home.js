0768acff06bbf005a88ab942310b7eb99af14258
sam liu <1145694956@qq.com>
2017-05-31 18:25:10 +0800

    import the "react-mobile-datepicker" component

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { inject, observer } from "mobx-react";
 import Button from "./UIComponents/Button";
+import DatePicker from "react-mobile-datepicker";
 
 
 @inject("store")
@@ -10,6 +11,10 @@ class Home extends Component {
     constructor(props){
         super(props);
         this.changeGlobalText=this.changeGlobalText.bind(this);
+        this.state={
+            time:new Date(),
+            isOpen:false
+        }
     }
 
     changeGlobalText(){
@@ -17,13 +22,41 @@ class Home extends Component {
         this.props.store.changeGlobalText(newText);
     }
 
+    handleOpenDatePicker=() => {
+        this.setState({
+           isOpen: true
+        })
+    }
+
+    handleCancel = () => {
+		this.setState({ isOpen: false });
+	}
+
+	handleSelect = (time) => {
+		this.setState({ time, isOpen: false });
+	}
+
+    dateFormater(time){
+        return `${time.getFullYear()}年${time.getMonth() + 1}月${time.getDate()}日`;
+    }
+
     render() {
+        //console.log(this.state.time);
+
         return (
             <div>
                 <img src={require("../image/test.jpg")} alt=""/>
-                This is Home Page.<br/>
+                {this.dateFormater(this.state.time)}<br/>
                 <input type="text" ref="globalText"/>
-                <Button amStyle="primary" block radius onClick={this.changeGlobalText}>change globalText</Button>
+                <Button amStyle="primary" block radius onClick={this.handleOpenDatePicker}>change globalText</Button>
+                <DatePicker
+					value={this.state.time}
+					isOpen={this.state.isOpen}
+					onSelect={this.handleSelect}
+					onCancel={this.handleCancel} 
+                    theme="ios"
+                    dateFormat={["YYYY","M","D"]}
+                />
             </div>
         );
     }
